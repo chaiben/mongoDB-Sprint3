e0dc54ef23b48bd75673207071a514a48d1e2ab2
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-10-18 07:47:51 +0200

    Middleware WIP

--- entrega2_nivel1/app.js
+++ entrega2_nivel1/app.js
@@ -43,7 +43,7 @@ console.log(square.multiply({a: 2, b:3}));
 const cube = new Middlewares(calculator);
 
 cube.use((req, next) => {
-  console.log(req)
+  console.log('Cube of', req)
   next();
 });
 
@@ -63,3 +63,28 @@ cube.use((req, next) => {
 console.log(cube.add({a: 5, b: 10}));
 console.log(cube.subtract({a: 10, b: 6}));
 console.log(cube.multiply({a: 2, b:3}));
+
+// Divide function
+const divide = new Middlewares(calculator);
+
+divide.use((req, next) => {
+  console.log('divide of', req)
+  next();
+});
+
+divide.use((req, next) => {
+  req.a = req.a / req.b
+  console.log(req)
+  next();
+});
+
+divide.use((req, next) => {
+  req.b = req.b ** 2
+  console.log(req)
+  next();
+});
+
+
+console.log(divide.add({a: 5, b: 10}));
+console.log(divide.subtract({a: 10, b: 6}));
+console.log(divide.multiply({a: 2, b:3}));
